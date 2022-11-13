d49ea2b47694995a250d5cbe467cb40a4c0db7c7
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-11-13 23:51:26 +0100

    updated with real datas

--- src/app.js
+++ src/app.js
@@ -22,41 +22,60 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   
             <div class="col-2">
-              <p class="forecst">${day}</p>
+              <p class="forecst">${formatDay(forecastDay.time)}</p>
               <img
-                src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+                src="${forecastDay.condition.icon_url}"
                 alt=""
                 width="42"
                 class="forecstImg"
               />
 
               <p class="forecst temp">
-                <span class="maxTemp">18°</span>
-                <span class="minTemp">12°</span>
+                <span class="maxTemp">${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</span>
+                <span class="minTemp">${Math.round(
+                  forecastDay.temperature.minimum
+                )}°</span>
               </p>
             </div>
           
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "453efefad2a2c05ta857fb9cb92548o9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayClimate(response) {
-  console.log(response.data);
   let temperatureEvent = document.querySelector("#temperature");
   let cityNameEvent = document.querySelector("#cityName");
   let humidityEvent = document.querySelector("#humidity");
@@ -64,8 +83,6 @@ function displayClimate(response) {
   let descriptionEvent = document.querySelector("#description");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-
-  displayForecast();
 
   const { city, country, temperature, condition, wind, time } = response.data;
 
@@ -79,6 +96,8 @@ function displayClimate(response) {
   dateElement.innerHTML = formateDate(time * 1000);
   iconElement.setAttribute("src", condition.icon_url);
   iconElement.setAttribute("alt", condition.description);
+
+  getForecast(response.data.coordinates);
 }
 function search(city) {
   let apiKey = "453efefad2a2c05ta857fb9cb92548o9";
